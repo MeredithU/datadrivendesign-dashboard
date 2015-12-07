65779e1db0fc0670c7be0fabcf4f9fc1315a912d
David Turissini <david@Davids-MacBook-Pro.local>
2015-12-06 22:16:43 -0700

    Adding logout button

--- public/js/component/layout/default.js
+++ public/js/component/layout/default.js
@@ -2,12 +2,14 @@
 
 import React from 'react';
 
-export default function ({ user, page }) {
+export default function ({ user, page, onLogoutClick }) {
     return (
         <div>
             <header className="app-header container-fluid">
                 <div className="col-xs-4 col-sm-offset-8 text-right">
                     Hello, {user.get('username')}
+
+                    <a href="#" onClick={onLogoutClick}>Logout</a>
                 </div>
             </header>
             <main className="app-page">
--- public/js/controller/layout/default.js
+++ public/js/controller/layout/default.js
@@ -3,6 +3,8 @@
 import React from 'react';
 import rx from 'rx';
 
+// Client
+import requestAbTest from 'dashboard/action/requestAbTest';
 
 // Stream
 import currentUserStream from 'dashboard/stream/user/current';
@@ -24,11 +26,47 @@ export default function (route, page) {
             return userSession;
         })
         .flatMapLatest((userSession) => {
-            return currentUserStream;
+            return currentUserStream
+                .map((user) => {
+                    return { user, userSession }
+                });
         })
-        .map((user) => {
-            return defaultLayout({ user, page });
-        });
+        .map(({ user, userSession }) => {
+
+            function onLogoutClick (e) {
+                e.preventDefault();
+                const userSessionToken = userSession._id;
+                const userId = user.get('_id');
+
+                requestAbTest('/userSession', {
+                    method: 'delete',
+                    headers: {
+                        authentication: `token ${userId}:${userSessionToken}`
+                    }
+                })
+                .subscribe(
+                    function (resp) {
+                        console.log('success');
+                    },
+                    function (err) {
+                        console.log('error');
+                    }
+                );
+
+
+            }
+
+            return {
+                user,
+                page,
+                onLogoutClick
+            }
+        })
+        .map((props) => {
+            return defaultLayout({
+                ...props
+            });
+        })
         
 
     return rx.Observable.merge(noLayoutStream, layoutStream);
