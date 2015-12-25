81305e2b24e7555d8c0d5d765707ade15e73cc75
davidturissini <davidairborne@gmail.com>
2015-12-24 21:33:39 -0500

    Updating package json

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 
 const express = require('express');
 const fs = require('fs');
-const port = 8888;
+const port = 80;
 const mustache = require('mustache')
 
 const apporigin = process.env.DASHBOARD_ABTEST_APP_ORIGIN;
